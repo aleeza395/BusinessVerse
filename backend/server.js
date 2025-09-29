643ed9c74294e3f8ccf0fe76914feabceda21cbf
aleeza395 <aliza25300@gmail.com>
2025-09-29 12:32:15 -0700

    updated server

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send("Backend is running ")
+})
+
+
 const normalizeCategory = (category) => category.trim().toLowerCase();
 
 app.post("/api/:category", (req, res) => {
